9d768f945fb6ab4423aa2e53e4f17dfd143be927
Vitalii Tomenko <vitalikxxx985@gmail.com>
2023-09-17 12:29:07 +0200

    Change useState

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(() => {
-    const storedTasks = localStorage.getItem('tasks');
-    return storedTasks ? JSON.parse(storedTasks) : [
-
-    ];
-  });
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks')) || []);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
