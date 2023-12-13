703a445ac6601da48302dae6cb22fbce6ef18ce4
Vitalii Tomenko <vitalikxxx985@gmail.com>
2023-12-13 21:42:35 +0100

    some changes in styled

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -4,6 +4,7 @@ export const StyledSection = styled.section`
     margin: 10px 0;
     background: ${({ theme }) => theme.background.backgroundColors};
     box-shadow: 0 0 5px #ddd;
+    text-decoration: none;
 `;
 
 export const Header = styled.header`
