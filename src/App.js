c41b9a41941997582426c2b4aae85e2853bdbd50
Vitalii Tomenko <vitalikxxx985@gmail.com>
2023-12-13 21:40:54 +0100

    created Navigation

--- src/App.js
+++ src/App.js
@@ -2,33 +2,25 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom/cjs/react-
 import TasksPage from "./features/tasks/TasksPage/index";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
-import { StyledNavLink } from "./styled";
+import Navigation from "./Navigation";
+import { toAuthor, toTask, toTasks } from "./routes";
 const App = () => (
     <HashRouter>
-        <nav>
-            <ul>
-                <li>
-                    <StyledNavLink to="/zadania">Zadania </StyledNavLink>
-                </li>
-                <li>
-                    <StyledNavLink to="/autor">O autorze </StyledNavLink>
-                </li>
-            </ul>
-            <Switch>
-                <Route path="/zadania/:id">
-                    <TaskPage />
-                </Route>
-                <Route path="/zadania">
-                    <TasksPage />
-                </Route>
-                <Route path="/autor">
-                    <AuthorPage />
-                </Route>
-                <Route path="/">
-                    <Redirect to="/zadania" />
-                </Route>
-            </Switch>
-        </nav>
+        <Navigation />
+        <Switch>
+            <Route path={toTask()}>
+                <TaskPage />
+            </Route>
+            <Route path={toTasks()}>
+                <TasksPage />
+            </Route>
+            <Route path={toAuthor()}>
+                <AuthorPage />
+            </Route>
+            <Route>
+                <Redirect to={toTasks()} />
+            </Route>
+        </Switch>
     </HashRouter>
 );
 
